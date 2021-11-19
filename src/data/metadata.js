56c138957af91f1cf3749ce7ae8dc149bbce44b3
Donny Burnside <donnyburnside1@gmail.com>
2021-11-19 01:26:57 +0000

    Added Google Analytics ID to metadata

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -3,5 +3,6 @@ module.exports = () => {
         title: 'Donny Burnside',
         description: 'Web Developer based in Hull and the East Riding of Yorkshire.',
         url: 'http://www.donnyburnside.com',
+        google_analytics_id: 'UA-46000033-20',
     }
 }
