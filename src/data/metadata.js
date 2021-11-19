3cad642720ed9f429ed52fab0f0ec9647384989e
Donny Burnside <donnyburnside1@gmail.com>
2021-11-19 01:16:42 +0000

    Added 'url' to metadata

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -2,5 +2,6 @@ module.exports = () => {
     return {
         title: 'Donny Burnside',
         description: 'Web Developer based in Hull and the East Riding of Yorkshire.',
+        url: 'http://www.donnyburnside.com',
     }
 }
